8a6983308841e1a5eaf76025d1df4d6e129e8112
Alexis <bzdalexis@gmail.com>
2023-03-07 10:15:38 +0100

    Initial Commit

---
+++ app_covoit/app_covoitApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_covoitApp.swift
+//  app_covoit
+//
+//  Created by Alexis BOIZARD on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct app_covoitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
